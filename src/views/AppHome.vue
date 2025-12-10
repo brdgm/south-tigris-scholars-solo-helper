f4fc9e904d7d4a29c6936c2a15b6070ff17d259d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-12-10 22:36:53 +0100

    Add BGG Link

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -11,13 +11,13 @@
   <p v-html="t('home.play1')"></p>
   <p v-html="t('home.play2')"></p>
   <p v-html="t('home.play3')"></p>
-<!--
+
   <p class="text-muted fst-italic">
     {{t('home.feedback')}}
-    <a href="https://boardgamegeek.com/thread/XXX" target="_blank" rel="noopener">Board Game Geek Thread</a> |
+    <a href="https://boardgamegeek.com/thread/3625930/brdgmme-scholars-of-the-south-tigris-solo-helper-w" target="_blank" rel="noopener">Board Game Geek Thread</a> |
     <a href="https://github.com/brdgm/south-tigris-scholars-solo-helper" target="_blank" rel="noopener">GitHub</a>
   </p>
--->
+
   <router-link to="/setup" class="btn btn-primary btn-lg mt-3">{{t('action.playGame')}}</router-link>
 
   <p class="disclaimer">
